26edcb0bed5d68f6438d77e8928a0c99c3fc5f28
Andyalcam <andreaalcam@ciencias.unam.mx>
2021-10-30 16:57:22 -0500

    Queda listo el main, version final

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,34 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
         String directorio = "src/laberintos/";
 
-        /*Maze mazeA = new Maze(directorio+"LaberintoA.txt");
+        String opc;
+
+        boolean rep = false;
+
+        Scanner in = new Scanner(System.in);
+
+        Maze mazeA = new Maze(directorio+"LaberintoA.txt");
 
         Maze mazeB = new Maze(directorio+"LaberintoB.txt");
 
-        mazeA.begin();
-
-        mazeB.begin();*/
-
-        Maze mazeC = new Maze(directorio+"LaberintoC.txt");
-        mazeC.begin();
+        do{
+            System.out.println("*** BIENVENIDO ***");
+            System.out.println("\n¿Qué laberinto deseas probar?");
+            System.out.println("Laberinto A = A");
+            System.out.println("Laberinto B = B");
+            opc = in.nextLine();
+            if(opc.equalsIgnoreCase("A")){
+                mazeA.begin();
+            }else if(opc.equalsIgnoreCase("B")){
+                mazeB.begin();
+            }else{
+                System.out.println("\nEscribe 'A' o 'B'");
+                rep = true;
+            }
+        }while(rep);
 
     }
 
